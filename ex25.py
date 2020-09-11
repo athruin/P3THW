76b924e5f2e6be34ca7950334b6791f6cd402ff8
Brandon Lee <brandon@athruin.com>
2020-09-11 07:35:17 -0600

    Finished commenting ex25.

--- ex25.py
+++ ex25.py
@@ -8,23 +8,28 @@ def sort_words(words):
     return sorted(words)
 
 def print_first_word(words):
+    """Prints the first word."""
     word = words.pop(0)
     print(word)
 
 def print_last_word(words):
+    """Prints the last word."""
     word = words.pop(-1)
     print(word)
 
 def sort_sentence(sentence):
+    """Turns a sentence in words, then sorts the words."""
     words = break_words(sentence)
     return sort_words(words)
 
 def print_first_and_last(sentence):
+    """Prints the first and last word of the sentence."""
     words = break_words(sentence)
     print_first_word(words)
     print_last_word(words)
 
 def print_first_and_last_sorted(sentence):
+    """Prints the first and last word of the sentence, sorted."""
     words = sort_sentence(sentence)
     print_first_word(words)
     print_last_word(words)
